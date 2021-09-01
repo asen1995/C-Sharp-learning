5a592b6913b6004481e3b014bd9670887b3e4ff6
Asen Nikolaev <asen.nikolaev95@abv.bg>
2021-09-01 12:58:19 +0300

    testing C# threads

---
+++ multithreading/MultiThreadingDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+namespace FirstTimeExperience_C_Sharp.multithreading
+{
+    class MultiThreadingDemo
+    {
+        public static void startMultiThreadingDemo()
+        {
+            ThreadStart childref = new ThreadStart(PrintNameThread.CallToChildThread);
+
+
+            Console.WriteLine("In Main: Creating the Child thread");
+
+            Thread childThread = new Thread(childref);
+            childThread.Start();
+
+            //stop the main thread for some time to test abort 
+            Thread.Sleep(2000);
+
+            //now abort the child
+            Console.WriteLine("In Main: Aborting the Child thread");
+            try
+            {
+                childThread.Abort();
+
+            }catch (PlatformNotSupportedException e)
+            {
+                Console.WriteLine("PlatformNotSupportedException occured : " + e );
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ multithreading/PrintNameThread.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+
+namespace FirstTimeExperience_C_Sharp.multithreading
+{
+    class PrintNameThread
+    {
+
+        public static void CallToChildThread()
+        {
+            try
+            {
+                Console.WriteLine("Child thread starts");
+
+                for (int counter = 0; counter <= 10; counter++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine(counter);
+                }
+
+                Console.WriteLine("Child Thread Completed");
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine("Thread Abort Exception");
+            }
+            finally
+            {
+                Console.WriteLine("Couldn't catch the Thread Exception");
+            }
+        }
+
+    }
+}
